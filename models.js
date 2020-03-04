d1eaa00a452595a3b5957b19b8a120004315036a
CaiY77 <caih.yang@gmail.com>
2020-03-04 17:58:58 -0500

    setting up db tables

--- models.js
+++ models.js
@@ -3,5 +3,42 @@ const { Sequelize } = require('sequelize')
 const db = new Sequelize({
   database: 'ttp_stocks',
   dialect: 'postgres',
-  define: { underscored: true }
+  define:{
+      underscored: true,
+      returning: true
+  }
 });
+
+const User = db.define('users', {
+
+  fullname:Sequelize.STRING,
+
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password:Sequelize.STRING,
+
+  wallet:{
+    type: Sequelize.INTEGER,
+    defaultValue: 5000
+  }
+
+});
+
+const Stock = db.define('stocks', {
+  company: Sequelize.STRING,
+  ticker: Sequelize.STRING,
+  qty: Sequelize.INTEGER,
+})
+
+User.hasMany(Stock, {
+  onDelete: 'cascade',
+});
+
+Stock.belongsTo(User)
+
+module.exports = { User, Stock, db };
