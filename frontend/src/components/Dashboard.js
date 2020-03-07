65b118326a98d10996d1fd40e3bb5cb790bd484d
CaiY77 <caih.yang@gmail.com>
2020-03-07 16:36:13 -0500

    adding portfolio components and setting up react router

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import {Route,Link} from 'react-router-dom';
+import Portfolio from './Portfolio.js'
+import Transaction from './Transaction.js'
 
 class Dashboard extends Component {
-
+constructor(props) {
+  super(props);
+  this.state = {
+    active: "portfolio"
+  };
+}
   render() {
-    return (
-      <div>
-        
-      </div>
-    );
+    return (<div>
+      <nav>
+        <Link className="nav-item" to = "/"> <div>Portfolio</div></Link>
+        <Link className="nav-item" to = "/transactions"> <div>Transactions</div></Link>
+      </nav>
+
+      <Route exact path = '/' render={()=> <Portfolio/>}/>
+      <Route path = '/transactions' render={()=> <Transaction/>}/>
+
+    </div>);
   }
 
 }
