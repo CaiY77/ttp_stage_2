44baa08a85bbce22b37b75096c0ae16b8ab0dda3
CaiY77 <caih.yang@gmail.com>
2020-03-09 05:51:06 -0400

    error fix

--- models.js
+++ models.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/ttp_stocks'{
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/ttp_stocks',{
   database: 'ttp_stocks',
   dialect: 'postgres',
   define:{
