0b5bca694c8588dcdad6ec601ec5d7b6ffdaba26
CaiY77 <caih.yang@gmail.com>
2021-04-12 11:56:14 -0400

    second attempt

--- models.js
+++ models.js
@@ -8,7 +8,8 @@ const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/
       returning: true
   },
   dialectOptions:{
-    ssl:true
+    ssl:true,
+    rejectUnauthorized:false
   }
 });
 
